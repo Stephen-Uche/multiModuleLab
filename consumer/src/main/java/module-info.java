f45004752165e6adacf4892294b7a605261d3da4
Osedumme Steven <stephen.osedumme@iths.se>
2023-05-04 07:53:04 +0200

    Created project module-info

---
+++ consumer/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module org.example.consumer {
+
+    requires java.base;
+    requires org.example.service;
+
+    uses org.example.service.Calculator;
+}
---
+++ provider/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+import org.example.provider.AdditionCalculator;
+import org.example.provider.MultiplicationCalculator;
+import org.example.provider.SubtractionCalculator;
+import org.example.service.Calculator;
+
+module org.example.provider {
+    requires org.example.service;
+    exports org.example.provider;
+
+    provides Calculator with AdditionCalculator, MultiplicationCalculator, SubtractionCalculator;
+}
