f23f564762d52536e37b553ab4d2cd8a133e8490
Osedumme Steven <stephen.osedumme@iths.se>
2023-05-04 07:57:36 +0200

    Created annotated calculator class

---
+++ provider/src/main/java/org/example/provider/AnnotatedCalculator.java
@@ -0,0 +1,10 @@
+package org.example.provider;
+
+import org.example.service.annotation.Calculation;
+
+@Calculation(value = (12+8))
+public class AnnotatedCalculator {
+    public int calculateNumbers() {
+        return calculateNumbers();
+    }
+}
